0bc904a4a5d48f4119ae85624e7927d6feb23ac2
AkshayParate <akshay.parate@somaiya.edu>
2020-07-21 21:20:23 +0530

    DELETE

--- src/com/foodpla/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.foodpla.dao;
-
-public interface LoginDao 
-{
-	boolean CustomerLogin(String emailid,String password);
-boolean CustomerChangePassword(String emailid,String password);
-boolean AdminLogin(String adminid,String adminpassword);
-boolean AdminChangepassword(String adminid,String adminpassword);
-}
