fdc00cf048c4369254d442cae99a428b4cbb4263
AkshayParate <akshay.parate@somaiya.edu>
2020-07-21 21:18:06 +0530

    UPDATE

--- src/com/foodpla/utility/DBUtility.java
+++ src/com/foodpla/utility/DBUtility.java
@@ -1,29 +1,20 @@
 package com.foodpla.utility;
 
 import java.sql.Connection;
-
-
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
 
 public class DBUtility 
 {
-	
-	
 	public static Connection getconnection()
 	{
-String DbPath = "jdbc:postgresql://" + "ec2-34-233-226-84.compute-1.amazonaws.com:5432" +"/d1b6kdr9k79m0a?sslmodel=require";
-String userid  = "fkfnvdojixbdda";
-String password = "8218bfe911d5a4b14f088fc9339896405dc30ff376139e5b266e798845afbce1";
+String DbPath = "jdbc:mysql://mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/FoodPla";
+String userid  = "hx8lzdfi4z15ho2e";
+String password = "sy2cnrfiky49vibx";
 Connection con = null; 
 try
 {
-Class.forName("org.postgresql.Driver").newInstance();;
+Class.forName("com.mysql.cj.jdbc.Driver");
 con= DriverManager.getConnection(DbPath,userid,password);
 }
 catch(ClassNotFoundException c)
@@ -34,13 +25,6 @@ catch(SQLException s)
 {
 s.printStackTrace();	
 }
-catch (InstantiationException e1) {
-	// TODO Auto-generated catch block
-	e1.printStackTrace();
-} catch (IllegalAccessException e1) {
-	// TODO Auto-generated catch block
-	e1.printStackTrace();
-} 
 return con;
 	}
 public static void main(String args[])
